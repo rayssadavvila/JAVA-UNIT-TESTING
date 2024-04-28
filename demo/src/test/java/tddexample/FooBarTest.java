ac364fe75b0af8385c15c08d10ba9564ec02c601
rayssadavvila <rayssamunizd@gmail.com>
2024-04-28 18:48:52 +0000

    Creating the first unit test

---
+++ demo/src/test/java/tddexample/FooBarTest.java
@@ -0,0 +1,13 @@
+package tddexample;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class FooBarTest {
+    
+    @Test
+    void test(){
+        System.out.println("Hello Junit 5!");
+    }
+}
