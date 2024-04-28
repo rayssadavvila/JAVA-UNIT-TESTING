207f95777a27819b4061fc8a712bcd6e6b53a339
rayssadavvila <rayssamunizd@gmail.com>
2024-04-28 20:43:05 +0000

    Implementing the class SimpleWords

---
+++ demo/src/main/java/tddexampleWords/SimpleWords.java
@@ -0,0 +1,12 @@
+package tddexampleWords;
+
+public class SimpleWords {
+    
+    String[] phrase = new String[4];
+    
+    phrase[0] = "Hi";
+    phrase[1] = "I";
+    phrase[2] = "am";
+    phrase[3] = "John";
+    
+}
---
+++ demo/src/test/java/tddExampleWords/SimpleWordsTest.java
@@ -0,0 +1,14 @@
+package tddExampleWords;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleWordsTest{
+
+    @Test
+    void verifyWordsTest(String word){
+        
+    }
+
+}
